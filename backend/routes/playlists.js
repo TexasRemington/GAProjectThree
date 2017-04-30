11f7065fae98ed789566f2bb85ab89291fa176a3
Remington Griffin <remington@utexas.edu>
2017-04-30 14:00:27 -0500

    fuck

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -8,16 +8,17 @@ var router = express.Router();
 
 var Music = require('../playlist/OceanF.m4a');
 
+var filepath = path.join(__dirname, './playlist/OceanF.m4a');
+console.log('filepath ', filepath);
+
 router.get('/playlists', function(req,res){
-  Playlist.find({}, function(err, playlists){
+
     if(err){ console.log(err); }
 
     res.set({'Content-Type': 'audio/mpeg'});
-    var readStream = fs.createReadStream(Music);
+    var readStream = fs.createReadStream(filepath);
     readStream.pipe(res);
   });
-});
-
 
 
 
